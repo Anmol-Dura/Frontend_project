67cb9d1dc52ca9f30b8f18a40369fc460bad1bf0
Anmol Dura <anmoldura8@gmail.com>
2024-03-20 19:24:36 -0400

    fixes a bug having the package nav element added multiple times in the desktop view resizing

--- js/java.js
+++ js/java.js
@@ -1,19 +1,22 @@
+let counter = 0; // Initialize counter variable outside the function
+
 function handleViewportChange() {
-    if (window.innerWidth <= 768) {
+    if (window.innerWidth >= 768 && counter === 0) {
+        // Desktop view and counter is 0
+        let nav = document.querySelector('.sidebar');
+        let newElement = document.createElement("div");
+        newElement.className = 'new-element'; // Add a class for easy identification
+        newElement.innerHTML = '<a href="./pages/packages.html">Packages</a>';
+        nav.appendChild(newElement);
+        counter = 1; // Increment counter to indicate the link has been appended
+    } else if (window.innerWidth < 768) {
         // Mobile view
-        // Remove the newly appended element if it exists
-        let nav = document.getElementsByClassName('sidebar')[0];
+        counter = 0; // Reset counter when viewport width falls below threshold
+        let nav = document.querySelector('.sidebar');
         let newElement = nav.querySelector('.new-element');
         if (newElement) {
             nav.removeChild(newElement);
         }
-    } else {
-        // Desktop view
-        let nav = document.getElementsByClassName('sidebar')[0];
-        let newElement = document.createElement("div");
-        newElement.className = 'new-element'; // Add a class for easy identification
-        newElement.innerHTML = '<a href="./pages/packages.html">package</a>';
-        nav.appendChild(newElement);
     }
 }
 
