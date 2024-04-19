110e3b39a2fd6abebaf37762572512f744084bc9
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-04-19 08:23:26 -0400

    AboutUs Page - Adding Functionality to Login Btn

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const toggleButton = document.getElementById('toggleDarkMode');
-//     const contactSections = document.querySelectorAll('.contact-info, .contact-form');
-//     const body = document.querySelector('body.contact-page');
-
-//     toggleButton.addEventListener('click', function() {
-//         contactSections.forEach(section => {
-//             section.classList.toggle('dark-mode');
-//         });
-
-//         // Toggle background image based on dark mode
-//         body.classList.toggle('dark-mode-background');
-//     });
-// });
-
 let logTab = document.getElementById('logout');
 document.addEventListener('DOMContentLoaded', function(){
     let userLogged = localStorage.getItem('islogged');
